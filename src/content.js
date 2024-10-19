77c08e273b0d54853e3f3b8e534a916800b073f6
Vaibhav Agarwal <vaibhavagarwal@Vaibhavs-MacBook-Pro.local>
2024-10-19 16:12:59 -0700

    Add a button on hover

--- src/content.js
+++ src/content.js
@@ -11,21 +11,58 @@ document.addEventListener("mouseup", () => {
   }
 });
 
-document.addEventListener("mouseup", () => {
-  console.log("This was mouseup");
+document.addEventListener("mouseup", (event) => {
   const selectedText = window.getSelection().toString();
   if (selectedText !== "") {
     highlightedText = selectedText;
-    chrome.runtime.sendMessage(
-      { action: "asl-to-video", data: selectedText },
-      (response) => {
-        console.log("This is in content.js");
-        debugger;
-        console.log("response received from gemini", response);
-      }
-    );
+    console.log("This was mouseup");
+    showConvertButton(event);
   } else {
     highlightedText = null;
+    hideConvertButton();
     // chrome.runtime.sendMessage({ action: "hideButton" });
   }
 });
+
+function showConvertButton(event) {
+  // below the mouse up location, show a button
+  let convertButton = document.querySelector("#convert-button");
+  if (!convertButton) {
+    convertButton = document.createElement("button");
+    convertButton.textContent = "Show ASL!";
+    convertButton.style.display = "none";
+    document.body.appendChild(convertButton);
+  }
+
+  convertButton.setAttribute("id", "convert-button");
+  convertButton.setAttribute("style", "background:red");
+  convertButton.style.position = "absolute";
+  convertButton.style.top = `${event.clientY + 20}px`; // Adjust the offset as needed
+  convertButton.style.left = `${event.clientX - 20}px`;
+  convertButton.style.display = "block";
+
+  // Add the button to the document body
+
+  convertButton.addEventListener("click", onConvertButtonClick);
+}
+
+function hideConvertButton() {
+  let convertButton = document.querySelector("#convert-button");
+  if (convertButton) {
+    convertButton.style.display = "none";
+  }
+}
+
+function onConvertButtonClick() {
+  // callServiceWorkerForTranslation();
+  // showLoader();
+  chrome.runtime.sendMessage(
+    { action: "asl-to-video", data: highlightedText },
+    (response) => {
+      // hideLoader();
+      // playVideo();
+      console.log("This is in content.js");
+      console.log("response received from gemini", response);
+    }
+  );
+}
