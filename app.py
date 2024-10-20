16707616eb88dbe9b2e530b650679952a8ec9912
shubhambanavalikar <shubhamappdev@gmail.com>
2024-10-20 10:58:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, send_file, jsonify
 
+from flask_cors import CORS
 import requests
 from pose_format import Pose
 from pose_format.pose_visualizer import PoseVisualizer
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/run', methods=['POST'])
 def translate_text():
