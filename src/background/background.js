9c3aece146cab421acf4acb543c66a12ca5aff03
Vaibhav Agarwal <vaibhavagarwal@Vaibhavs-MacBook-Pro.local>
2024-10-19 18:10:04 -0700

    improve image

--- src/background/background.js
+++ src/background/background.js
@@ -1,12 +1,12 @@
 console.log("This is background.js");
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "printSelectedText") {
-    const selectedText = window.getSelection().toString();
-    window.print(selectedText);
-  }
-  sendResponse();
-});
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "printSelectedText") {
+//     const selectedText = window.getSelection().toString();
+//     window.print(selectedText);
+//   }
+//   sendResponse();
+// });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   response = {};
@@ -15,15 +15,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     response = callGemini(request.data);
   }
   console.log("Returning response from bg.js: ", response);
-  sendResponse({ response: "geminiResponse" });
+  sendResponse(response);
+  return true;
 });
 
 function callGemini(text) {
+  for (let i = 0; i < 100; i++) {}
   // https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose?text=I ate an omelet for breakfast today and plan to eat pasta for lunch.&spoken=en&signed=ase
   // const getVideoUrl = "https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose?spoken=en&signed=ase&text=" + text;
-  return "hello from gemini";
-  //   return {
-  //     text,
-  //     video: "posed video",
-  //   };
+  // return "hello from gemini";
+  return {
+    text,
+    video: "http://localhost:8000/example.mp4",
+  };
 }
---
+++ src/content/content.js
@@ -0,0 +1,143 @@
+console.log("This is content.js");
+var highlightedText = null;
+var convertButtonVisible = false;
+document.addEventListener("mouseup", (event) => {
+  const selectedText = window.getSelection().toString();
+  if (convertButtonVisible) return;
+  if (selectedText !== "") {
+    highlightedText = selectedText;
+    console.log("This was mouseup");
+    showConvertButton(event);
+    convertButtonVisible = true;
+  } else {
+    highlightedText = null;
+    hideAllExtraDivs();
+    // chrome.runtime.sendMessage({ action: "hideButton" });
+  }
+});
+
+function hideAllExtraDivs() {
+  hideConvertButton();
+  hideLoader();
+  hideOutputDiv();
+  convertButtonVisible = false;
+}
+
+function hideOutputDiv() {
+  const outputDiv = document.querySelector("#output-div");
+  if (outputDiv) {
+    outputDiv.remove();
+  }
+}
+
+function getEventCoordinates(event) {
+  return {
+    left: event.clientX,
+    top: event.clientY,
+  };
+}
+function showConvertButton(event) {
+  // below the mouse up location, show a button
+  let convertButton = document.querySelector("#convert-button");
+  if (!convertButton) {
+    convertButton = document.createElement("button");
+    convertButton.innerHTML = `
+    <img height="25px" width="25px" style="display:inline-block;" src="https://media.istockphoto.com/id/1311595752/vector/sign-language-interpreting-vector-icon-us-sing-language-hand-symbol-isolated-on-white.jpg?s=612x612&w=0&k=20&c=NxRdHaySIEOpXIQC3se_QJTJR65fCPP8BCMGTUcEkUU="/>
+    Show ASL!
+    `;
+    //convertButton.textContent = "Show ASL!";
+    convertButton.style.display = "none";
+    document.body.appendChild(convertButton);
+  }
+
+  convertButton.setAttribute("id", "convert-button");
+  positionItem(convertButton, getEventCoordinates(event), {
+    top: 20,
+    left: -20,
+  });
+  convertButton.style.display = "block";
+
+  // Add the button to the document body
+
+  convertButton.addEventListener("click", onConvertButtonClick);
+}
+
+function positionItem(item, coords, offset) {
+  item.style.position = "absolute";
+  item.style.top = `${coords.top + offset?.top}px`;
+  item.style.left = `${coords.left + offset?.left}px`;
+}
+
+function hideConvertButton() {
+  let convertButton = document.querySelector("#convert-button");
+  if (convertButton) {
+    convertButton.remove();
+  }
+}
+
+function onConvertButtonClick(event) {
+  callServiceWorkerForTranslation();
+  showLoader(event);
+}
+
+function showLoader(event) {
+  const loader = document.createElement("div");
+  loader.classList.add("loader");
+  loader.innerHTML = '<div class="loading-spinner"></div>';
+  positionItem(loader, getEventCoordinates(event), {
+    top: 20,
+    left: -20,
+  });
+  document.body.appendChild(loader);
+}
+
+function hideLoader() {
+  const loader = document.querySelector(".loader");
+  if (loader) {
+    loader.remove();
+  }
+}
+
+async function callServiceWorkerForTranslation() {
+  chrome.runtime.sendMessage(
+    { action: "asl-to-video", data: highlightedText },
+    (response) => {
+      hideLoader();
+      console.log("This is in content.js");
+      console.log("response received from gemini", response);
+      showAsl(response);
+    }
+  );
+}
+
+function showAsl(response) {
+  // let response = {
+  //   text: "highlighted text",
+  //   video: "posed video",
+  // };
+  // create a video playing div
+  const outputDiv = document.createElement("div");
+  outputDiv.setAttribute("id", "output-div");
+  outputDiv.classList.add("output");
+  outputDiv.innerHTML = `
+    <div>
+      <div>Highlighted Text: ${response.text}</div>
+      <div>Video:
+        <video height="100px" width="100px" src="${response.video}"></video>
+      </div>
+    </div>`;
+  // outputDiv.innerText = response.text;
+  // outputDiv.innerText += "<video src=" + response.video + "></video>";
+  document.body.appendChild(outputDiv);
+  positionItem(
+    outputDiv,
+    {
+      top: 0,
+      left: 0,
+    },
+    {
+      top: 0,
+      left: 0,
+    }
+  );
+}
